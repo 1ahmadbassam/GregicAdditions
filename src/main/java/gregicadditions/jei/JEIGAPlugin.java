25bb56a159ef3dca5df9ff2e2d59fcc028dd799b
Ahmad Bassam El Bizri <34858431+1ahmadbassam@users.noreply.github.com>
2024-05-20 03:43:25 +0300

    include descriptions for multiblocks in JEI page

--- src/main/java/gregicadditions/jei/JEIGAPlugin.java
+++ src/main/java/gregicadditions/jei/JEIGAPlugin.java
@@ -2,14 +2,17 @@ package gregicadditions.jei;
 
 import gregicadditions.machines.GATileEntities;
 import gregicadditions.recipes.GARecipeMaps;
+import gregtech.integration.jei.multiblock.MultiblockInfoPage;
 import gregtech.integration.jei.multiblock.MultiblockInfoRecipeWrapper;
 import gregtech.integration.jei.recipe.primitive.CokeOvenRecipeWrapper;
 import mezz.jei.api.IModPlugin;
 import mezz.jei.api.IModRegistry;
 import mezz.jei.api.JEIPlugin;
+import mezz.jei.api.ingredients.VanillaTypes;
 import mezz.jei.api.recipe.IRecipeCategoryRegistration;
 
 import java.util.Arrays;
+import java.util.List;
 import java.util.stream.Collectors;
 
 @JEIPlugin
@@ -20,16 +23,24 @@ public class JEIGAPlugin implements IModPlugin {
     }
     @Override
     public void register(IModRegistry registry) {
-        registry.addRecipes(Arrays.asList(
-                new MultiblockInfoRecipeWrapper(new CokeOvenInfo()),
-                new MultiblockInfoRecipeWrapper(new AssemblyLineInfo()),
-                new MultiblockInfoRecipeWrapper(new FusionReactor1Info()),
-                new MultiblockInfoRecipeWrapper(new FusionReactor2Info()),
-                new MultiblockInfoRecipeWrapper(new FusionReactor3Info())),
+        List<MultiblockInfoPage> multiblocks = Arrays.asList(
+                new CokeOvenInfo(),
+                new AssemblyLineInfo(),
+                new FusionReactor1Info(),
+                new FusionReactor2Info(),
+                new FusionReactor3Info());
+        registry.addRecipes(multiblocks.stream()
+                        .map(MultiblockInfoRecipeWrapper::new)
+                        .collect(Collectors.toList()),
                 "gregtech:multiblock_info");
         registry.addRecipes(GARecipeMaps.COKE_OVEN_RECIPES.stream()
                 .map(CokeOvenRecipeWrapper::new)
                 .collect(Collectors.toList()), "gregtech:ga_coke_oven");
         registry.addRecipeCatalyst(GATileEntities.COKE_OVEN.getStackForm(), "gregtech:ga_coke_oven");
+        for (MultiblockInfoPage multiblock : multiblocks) {
+            registry.addIngredientInfo(multiblock.getController().getStackForm(),
+                    VanillaTypes.ITEM,
+                    multiblock.getDescription());
+        }
     }
 }
