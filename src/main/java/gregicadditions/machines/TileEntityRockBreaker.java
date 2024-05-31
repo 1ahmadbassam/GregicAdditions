0c1a3fbac186d4ec875dbff5618dc25724904aea
Ahmad Bassam El Bizri <34858431+1ahmadbassam@users.noreply.github.com>
2024-05-31 17:35:12 +0300

    add inventory for rock breakers and gui

--- src/main/java/gregicadditions/machines/TileEntityRockBreaker.java
+++ src/main/java/gregicadditions/machines/TileEntityRockBreaker.java
@@ -4,7 +4,9 @@ import codechicken.lib.render.CCRenderState;
 import codechicken.lib.render.pipeline.IVertexOperation;
 import codechicken.lib.vec.Matrix4;
 import gregtech.api.GTValues;
+import gregtech.api.gui.GuiTextures;
 import gregtech.api.gui.ModularUI;
+import gregtech.api.gui.widgets.SlotWidget;
 import gregtech.api.metatileentity.MetaTileEntity;
 import gregtech.api.metatileentity.MetaTileEntityHolder;
 import gregtech.api.metatileentity.TieredMetaTileEntity;
@@ -19,6 +21,7 @@ import net.minecraft.util.EnumFacing;
 import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.fml.common.Loader;
 import net.minecraftforge.items.IItemHandlerModifiable;
+import net.minecraftforge.items.ItemHandlerHelper;
 import net.minecraftforge.items.ItemStackHandler;
 
 import javax.annotation.Nullable;
@@ -30,6 +33,12 @@ public class TileEntityRockBreaker extends TieredMetaTileEntity {
     public TileEntityRockBreaker(ResourceLocation metaTileEntityId, OrientedOverlayRenderer renderer, int tier) {
         super(metaTileEntityId, tier);
         this.renderer = renderer;
+        this.initializeInventory();
+    }
+
+    private int getInventorySize() {
+        int sizeRoot = 1 + this.getTier();
+        return sizeRoot * sizeRoot;
     }
 
     @Override
@@ -39,7 +48,7 @@ public class TileEntityRockBreaker extends TieredMetaTileEntity {
 
     @Override
     protected IItemHandlerModifiable createExportItemHandler() {
-        return new ItemStackHandler(1);
+        return new ItemStackHandler(this.getInventorySize());
     }
 
     @Override
@@ -78,7 +87,7 @@ public class TileEntityRockBreaker extends TieredMetaTileEntity {
             long energyToConsume = GTValues.V[getTier()] / 16;
             int waitTime = (int) Math.ceil(32 / (Math.pow(2, getTier())));
             if (output != null && getOffsetTimer() % waitTime == 0 && energyContainer.getEnergyStored() >= energyToConsume) {
-                exportItems.insertItem(0, output, false);
+                ItemHandlerHelper.insertItemStacked(this.exportItems, output, false);
                 energyContainer.removeEnergy(energyToConsume);
             }
             if (getOffsetTimer() % 5 == 0) {
@@ -113,11 +122,17 @@ public class TileEntityRockBreaker extends TieredMetaTileEntity {
 
     @Override
     protected ModularUI createUI(EntityPlayer entityPlayer) {
-        return null;
-    }
+        int rowSize = (int)Math.sqrt(this.getInventorySize());
+        ModularUI.Builder builder = ModularUI.builder(GuiTextures.BACKGROUND, 176, 18 + 18 * rowSize + 94).label(10, 5, this.getMetaFullName());
 
-    @Override
-    protected boolean openGUIOnRightClick() {
-        return false;
+        for(int y = 0; y < rowSize; ++y) {
+            for(int x = 0; x < rowSize; ++x) {
+                int index = y * rowSize + x;
+                builder.widget((new SlotWidget(this.exportItems, index, 89 - rowSize * 9 + x * 18, 18 + y * 18, true, false)).setBackgroundTexture(GuiTextures.SLOT));
+            }
+        }
+
+        builder.bindPlayerInventory(entityPlayer.inventory, GuiTextures.SLOT, 7, 18 + 18 * rowSize + 12);
+        return builder.build(this.getHolder(), entityPlayer);
     }
 }
