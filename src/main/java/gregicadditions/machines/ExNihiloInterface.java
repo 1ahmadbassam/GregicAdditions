0701c6656fd359a7c1c065512df3c7ea1ab3e688
Ahmad Bassam El Bizri <34858431+1ahmadbassam@users.noreply.github.com>
2024-05-31 14:37:31 +0300

    remove XSTR instance creation and revert to GTUtility XSTR

--- src/main/java/gregicadditions/machines/ExNihiloInterface.java
+++ src/main/java/gregicadditions/machines/ExNihiloInterface.java
@@ -1,19 +1,17 @@
 package gregicadditions.machines;
 
 import exnihilocreatio.ModFluids;
-import gregtech.api.util.XSTR;
+import gregtech.api.util.GTUtility;
 import net.minecraft.init.Blocks;
 import net.minecraft.item.ItemStack;
 
 import javax.annotation.Nullable;
 
 public class ExNihiloInterface {
-    private static final XSTR random = new XSTR();
-
     @Nullable
     static ItemStack getWitchWaterDrop(TileEntityRockBreaker instance) {
         if (instance.checkSides(Blocks.WATER) && instance.checkSides(ModFluids.blockWitchwater)) {
-            return new ItemStack(Blocks.DIRT, 1, random.nextInt(3));
+            return new ItemStack(Blocks.DIRT, 1, GTUtility.getRandomIntXSTR(3));
         }
         return null;
     }
